8fc0108ade260de9e216de38080d3a8209390c57
marwanrashed <maro.isme2010@gmail.com>
2021-02-17 22:14:02 -0500

    Plotly

--- lessons/WebDevelopment/FrontEndWorkspaceFiles/6_exercise_plotly/javascript_files/plot2.js
+++ lessons/WebDevelopment/FrontEndWorkspaceFiles/6_exercise_plotly/javascript_files/plot2.js
@@ -10,12 +10,34 @@ var country_name_china = 'China';
 
 var trace1 = {
 /* TODO: Use the country name and arable land data to make a bar chart */
+ x: year,
+ y: arable_land_brazil,
+ type: "bar",
+ name: country_name_brazil
+};
+
+var trace2 = {
+/* TODO: Use the country name and arable land data to make a bar chart */
+ x: year,
+ y: arable_land_germany,
+ type: "bar",
+ name: country_name_germany
+};
+
+var trace3 = {
+/* TODO: use the country name and arable land data to make a bar chart */
+ x: year,
+ y: arable_land_china,
+ type: "bar",
+ name: country_name_china
 };
 
 var layout = {
  /* TODO: include a title for the chart */
+ title: "Hectares Arable Land per Person 2015",
+  type: "bar"
 };
 
-var data = [trace1];
+var data = [trace1, trace2, trace3];
 
 Plotly.newPlot('plot2', data, layout);
